763605da7dd7f003117018822dc1d75b59f2f116
KavinduTheekshana <kavindutheekshana@gmail.com>
2019-04-14 15:05:12 +0530

    Aprial14Final

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -708,24 +708,24 @@
                 <div class="post-box blue-bg">
                     <div class="post-img"></div>
                     <span class="badge badge-danger">Hot</span>
-                    <div class="post-title">Discover our new website builder</div>
-                    <div class="post-link"><a href="#">Learn more</a></div>
+                    <div class="post-title">Microsoft’s HoloLens 2 with...</div>
+                    <div class="post-link"><a href="Blog">Learn more</a></div>
                 </div>
             </div>
             <div class="col-sm-6 col-md-4" data-animscroll="fade-in" data-animscroll-delay="400">
                 <div class="post-box grey-bg">
                     <div class="post-img"></div>
                     <span class="badge badge-info">Hot</span>
-                    <div class="post-title">New Data center in Germany & USA</div>
-                    <div class="post-link"><a href="#">Learn more</a></div>
+                    <div class="post-title">Incognito Mode Won’t Keep Your...</div>
+                    <div class="post-link"><a href="Blog">Learn more</a></div>
                 </div>
             </div>
             <div class="col-sm-12 col-md-4" data-animscroll="fade-in" data-animscroll-delay="600">
                 <div class="post-box yellow-bg">
                     <div class="post-img"></div>
                     <span class="badge badge-dark">Hot</span>
-                    <div class="post-title">Discover our newwebsite builder</div>
-                    <div class="post-link"><a href="#">Learn more</a></div>
+                    <div class="post-title">Microsoft's Edge Goes With...</div>
+                    <div class="post-link"><a href="Blog">Learn more</a></div>
                 </div>
             </div>
         </div>
